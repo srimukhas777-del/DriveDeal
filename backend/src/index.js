fbcf68c1106cc1b6979c81621098c82788d70e0f
unknown <srimukhas777@gmail.com>
2026-01-11 16:47:03 +0530

    corrected client url and test config

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,13 +2,15 @@ import app from './app.js';
 import connectDB from './config/db.js';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.MONGO_URI || 'http://localhost:3000',
+    origin: process.env.CLIENT_URL || 'http://localhost:3000',
     methods: ['GET', 'POST'],
   },
 });
